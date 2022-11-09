3f104eb8fec1ed93dba95daa19cb0cb5dc47fe52
clarayuan <c_w_yuan@hotmail.com>
2022-11-09 08:03:56 -0500

    finished video

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered: ", input)
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
-
+	fmt.Print("Enter number: ")
+	numInput, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number: ", aFloat)
+	}
 }
